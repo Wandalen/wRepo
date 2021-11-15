301a8f240e61a2c0aef4debef6ffcaf5dc072079
dmvict <dm.vict.kr@gmail.com>
2021-11-15 15:57:58 +0200

    Implement prototype of routine `commandMigrate`

--- proto/wtools/atop/repo/l8/Cui.s
+++ proto/wtools/atop/repo/l8/Cui.s
@@ -83,6 +83,7 @@ function _commandsMake()
     'version' :                 { ro : _.routineJoin( cui, cui.commandVersion ) },
 
     'agree' :                   { ro : _.routineJoin( cui, cui.commandAgree ) },
+    'migrate' :                 { ro : _.routineJoin( cui, cui.commandMigrate ) },
   };
 
   let ca = _.CommandsAggregator
@@ -180,6 +181,60 @@ command.properties =
   only : 'A pattern or array of patterns to include in merge. Could be a glob.',
 };
 
+//
+
+function commandMigrate( e )
+{
+  const properties = e.propertiesMap;
+
+  _.sure( _.str.defined( properties.srcPath ), 'Expects path to source repository.' );
+  _.sure( _.str.defined( properties.startState ), 'Expects start state to migrate commits.' );
+  _.mapSupplementNulls( properties, commandMigrate.defaults );
+
+  let onCommitMessage = properties.onMessage;
+  if( onCommitMessage )
+  onCommitMessage = require( _.path.join( _.path.current(), onCommitMessage ) );
+  let onDate = properties.onMessage;
+  if( onDate )
+  onDate = require( _.path.join( _.path.current(), onDate ) );
+
+  return _.git.repositoryMigrate
+  ({
+    srcPath : properties.srcPath || e.subject,
+    localPath : properties.localPath || _.path.current(),
+    state1 : properties.startState,
+    state2 : properties.endState,
+    srcBase : properties.srcBase,
+    dstBase : properties.dstBase,
+    onCommitMessage,
+    onDate,
+    but : properties.but,
+    only : properties.only,
+  })
+}
+
+var command = commandMigrate.command = Object.create( null );
+commandMigrate.defaults =
+{
+  srcBase : '.',
+  dstBase : '.',
+};
+command.hint = 'Migrate commits from one repository to another repository.';
+command.subjectHint = 'Path to source repository.';
+command.properties =
+{
+  srcPath : 'A path to source repository.',
+  dstPath : 'A local path to destination repository. Default is current directory.',
+  startState : 'A start commit.',
+  endState : 'An end commit.',
+  srcBase : 'A base directory for source repository. Filters changes in source repository in relation to this path. Default is source repository root directory.',
+  dstBase : 'A base directory for destination repository. Checks difference in relation to this path. Default is destination repository root directory.',
+  onMessage : 'A path to script that produce commit message. An original commit message will be passed to script. By default, command does not change commit message.',
+  onDate : 'A path to script that produce commit date. An original string date will be passed to script. By default, command does not change commit date.',
+  but : 'A pattern or array of patterns to exclude from merge. Could be a glob.',
+  only : 'A pattern or array of patterns to include in merge. Could be a glob.',
+};
+
 // --
 // relations
 // --
@@ -234,6 +289,7 @@ let Extension =
   // migrate
 
   commandAgree,
+  commandMigrate,
 
   // relations
 
