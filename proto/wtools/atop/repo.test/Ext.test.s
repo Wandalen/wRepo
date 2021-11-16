c3adb6c43fbf28f5b0f0f74d5f2bf3ac1e4df85a
dmvict <dm.vict.kr@gmail.com>
2021-11-16 12:05:23 +0200

    Write trivial test routine `migrate`

--- proto/wtools/atop/repo.test/Ext.test.s
+++ proto/wtools/atop/repo.test/Ext.test.s
@@ -56,6 +56,10 @@ function agree( test )
   a.ready.then( ( op ) =>
   {
     test.identical( op.exitCode, 0 );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'was.package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
     return null;
   });
   a.shell( 'git log -n 1' );
@@ -63,11 +67,6 @@ function agree( test )
   {
     test.identical( op.exitCode, 0 );
     test.identical( _.strCount( op.output, /Merge branch \'master\' of .*\/repo into master/ ), 1 );
-
-    var config = a.fileProvider.fileReadUnknown( a.abs( 'package.json' ) );
-    test.identical( config.name, 'wmodulefortesting2' );
-    var config = a.fileProvider.fileReadUnknown( a.abs( 'was.package.json' ) );
-    test.identical( config.name, 'wmodulefortesting2' );
     return null;
   });
 
@@ -83,6 +82,12 @@ function agree( test )
   a.ready.then( ( op ) =>
   {
     test.identical( op.exitCode, 0 );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    test.identical( config.version, '0.0.170' );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'was.package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    test.identical( config.version, '0.0.170' );
     return null;
   });
   a.shell( 'git log -n 1' );
@@ -90,13 +95,86 @@ function agree( test )
   {
     test.identical( op.exitCode, 0 );
     test.identical( _.strCount( op.output, /Merge branch \'master\' of .*\/repo.* into master/ ), 1 );
+    return null;
+  });
 
+  /* - */
+
+  return a.ready;
+
+  /* */
+
+  function begin()
+  {
+    a.ready.then( () => { a.fileProvider.filesDelete( a.abs( '.' ) ); return null });
+    a.ready.then( () => { a.fileProvider.filesDelete( a.abs( '../repo' ) ); return null });
+    a.ready.then( () => { a.fileProvider.dirMake( a.abs( '.' ) ); return null });
+    a.shell( `git clone ${ dstRepositoryRemote } ./` );
+    a.shell( `git reset --hard ${ dstCommit }` );
+    return a.shell( `git clone ${ srcRepositoryRemote } ../repo` );
+  }
+}
+
+//
+
+function migrate( test )
+{
+  const a = test.assetFor( false );
+  const dstRepositoryRemote = 'https://github.com/Wandalen/wModuleForTesting1.git';
+  const dstCommit = '8e2aa80ca350f3c45215abafa07a4f2cd320342a';
+  const srcRepositoryRemote = 'https://github.com/Wandalen/wModuleForTesting2.git';
+
+  /* - */
+
+  begin();
+  a.ready.then( () =>
+  {
+    test.case = 'migrate with local repository, start commit';
+    return null;
+  });
+  a.appStart( '.agree dst:./!master src:../repo#f68a59ec46b14b1f19b1e3e660e924b9f1f674dd' );
+
+  a.appStart( '.migrate dst:./!master src:../repo!master srcState1:#f68a59ec46b14b1f19b1e3e660e924b9f1f674dd' )
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
     var config = a.fileProvider.fileReadUnknown( a.abs( 'package.json' ) );
     test.identical( config.name, 'wmodulefortesting2' );
-    test.identical( config.version, '0.0.170' );
     var config = a.fileProvider.fileReadUnknown( a.abs( 'was.package.json' ) );
     test.identical( config.name, 'wmodulefortesting2' );
-    test.identical( config.version, '0.0.170' );
+    return null;
+  });
+  a.shell( 'git log -n 20' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    return null;
+  });
+
+  begin();
+  a.ready.then( () =>
+  {
+    test.case = 'migrate with local repository, start and end commits';
+    return null;
+  });
+  a.appStart( '.agree dst:./!master src:../repo#f68a59ec46b14b1f19b1e3e660e924b9f1f674dd' );
+
+  a.appStart( '.migrate dst:./!master src:../repo!master srcState1:#f68a59ec46b14b1f19b1e3e660e924b9f1f674dd srcState2:#d8c18d24c1d65fab1af6b8d676bba578b58bfad5' )
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    test.identical( config.version, '0.0.178' );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'was.package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    test.identical( config.version, '0.0.178' );
+    return null;
+  });
+  a.shell( 'git log -n 20' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
     return null;
   });
 
@@ -140,6 +218,7 @@ const Proto =
   tests :
   {
     agree,
+    migrate,
   },
 };
 
