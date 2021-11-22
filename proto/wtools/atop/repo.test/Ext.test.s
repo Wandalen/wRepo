f25bcff19d4fc53fb1260b3b0e6d6f90cac35d14
dmvict <dm.vict.kr@gmail.com>
2021-11-22 15:32:47 +0200

    Write test routine `migrateWithOptionDstDirPath`

--- proto/wtools/atop/repo.test/Ext.test.s
+++ proto/wtools/atop/repo.test/Ext.test.s
@@ -1742,7 +1742,11 @@ function migrateWithOptionSrcDirPath( test )
     filesBefore = a.find( a.abs( './' ) );
     return null;
   });
-  a.appStart( `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } srcDirPath:'.' onMessage:${ a.abs( '../OnMessage.js' ) }` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:'.' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
   a.shell( 'git diff --name-only HEAD~..HEAD' );
   a.ready.then( ( op ) =>
   {
@@ -1786,7 +1790,11 @@ function migrateWithOptionSrcDirPath( test )
     filesBefore = a.find( a.abs( './' ) );
     return null;
   });
-  a.appStart( `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } srcDirPath:'./' onMessage:${ a.abs( '../OnMessage.js' ) }` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:'./' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
   a.shell( 'git diff --name-only HEAD~..HEAD' );
   a.ready.then( ( op ) =>
   {
@@ -1830,7 +1838,11 @@ function migrateWithOptionSrcDirPath( test )
     filesBefore = a.find( a.abs( './' ) );
     return null;
   });
-  a.appStart( `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } srcDirPath:'./proto' onMessage:${ a.abs( '../OnMessage.js' ) }` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:'./proto' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
   a.shell( 'git diff --name-only HEAD~..HEAD' );
   a.ready.then( ( op ) =>
   {
@@ -1874,7 +1886,11 @@ function migrateWithOptionSrcDirPath( test )
     filesBefore = a.find( a.abs( './' ) );
     return null;
   });
-  a.appStart( `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } srcDirPath:'.' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:'.' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
   a.shell( 'git diff --name-only HEAD~..HEAD' );
   a.ready.then( ( op ) =>
   {
@@ -1918,7 +1934,11 @@ function migrateWithOptionSrcDirPath( test )
     filesBefore = a.find( a.abs( './' ) );
     return null;
   });
-  a.appStart( `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } srcDirPath:'./' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:'./' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
   a.shell( 'git diff --name-only HEAD~..HEAD' );
   a.ready.then( ( op ) =>
   {
@@ -1962,7 +1982,11 @@ function migrateWithOptionSrcDirPath( test )
     filesBefore = a.find( a.abs( './' ) );
     return null;
   });
-  a.appStart( `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } srcDirPath:'proto' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:'proto' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
   a.shell( 'git diff --name-only HEAD~..HEAD' );
   a.ready.then( ( op ) =>
   {
@@ -2021,6 +2045,205 @@ function migrateWithOptionSrcDirPath( test )
 }
 
 migrateWithOptionSrcDirPath.timeOut = 180000;
+
+//
+
+function migrateWithOptionDstDirPath( test )
+{
+  const a = test.assetFor( false );
+  const dstRepositoryRemote = 'https://github.com/Wandalen/wModuleForTesting1.git';
+  const dstCommit = '8e2aa80ca350f3c45215abafa07a4f2cd320342a';
+  const srcRepositoryRemote = 'https://github.com/Wandalen/wModuleForTesting2.git';
+  const srcState1 = 'f68a59ec46b14b1f19b1e3e660e924b9f1f674dd';
+  const srcState2 = 'd8c18d24c1d65fab1af6b8d676bba578b58bfad5';
+  const user = a.shell({ currentPath : __dirname, execPath : 'git config --global user.name', sync : 1 }).output.trim();
+
+  /* - */
+
+  let filesBefore;
+  begin().then( () =>
+  {
+    test.case = 'dstDirPath - current dir';
+    return null;
+  });
+  a.appStart( `.agree dst:./!master src:../repo#${ srcState1 }` );
+  a.ready.then( () =>
+  {
+    filesBefore = a.find( a.abs( './' ) );
+    return null;
+  });
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `dstDirPath:'.' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
+  a.shell( 'git diff --name-only HEAD~..HEAD' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    var files = op.output.trim().split( '\n' );
+    test.identical( files.length, 3 );
+
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    test.identical( config.version, '0.0.178' );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'was.package.json' ) );
+    test.identical( config.name, 'wmodulefortesting2' );
+    test.identical( config.version, '0.0.178' );
+    var config = a.fileProvider.fileReadUnknown( a.abs( 'will.yml' ) );
+    test.identical( config.about.name, 'wModuleForTesting2' );
+    test.identical( config.about.version, '0.1.0' );
+
+    var filesAfter = a.find( a.abs( './' ) );
+    test.identical( filesBefore, filesAfter );
+    return null;
+  });
+  a.shell( 'git log -n 20' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, '__sync__' ), 15 );
+    test.ge( _.strCount( op.output, `Author: ${ user }` ), 15 );
+    return null;
+  });
+
+  /* */
+
+  begin().then( () =>
+  {
+    test.case = 'dstDirPath - nested dir, dir synchronized';
+    return null;
+  });
+  a.appStart( `.agree dst:./!master src:../repo#${ srcState1 } dstDirPath:proto` );
+  a.ready.then( () =>
+  {
+    filesBefore = a.find( a.abs( './' ) );
+    return null;
+  });
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `dstDirPath:'proto' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
+  a.shell( 'git diff --name-only HEAD~..HEAD' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    var files = op.output.trim().split( '\n' );
+    test.identical( files.length, 3 );
+
+    var filesAfter = a.find( a.abs( './' ) );
+    test.identical( filesBefore, filesAfter );
+    return null;
+  });
+  a.shell( 'git log -n 20' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, '__sync__' ), 15 );
+    test.ge( _.strCount( op.output, `Author: ${ user }` ), 15 );
+    return null;
+  });
+
+  /* */
+
+  begin().then( () =>
+  {
+    test.case = 'dstDirPath - nested dir, srcDirPath, dir synchronized';
+    return null;
+  });
+  a.appStart( `.agree dst:./!master src:../repo#${ srcState1 } dstDirPath:proto` );
+  a.ready.then( () =>
+  {
+    filesBefore = a.find( a.abs( './' ) );
+    return null;
+  });
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `srcDirPath:proto dstDirPath:proto onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
+  a.shell( 'git diff --name-only HEAD~..HEAD' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    var files = op.output.trim().split( '\n' );
+    test.identical( files.length, 19 );
+
+    var filesAfter = a.find( a.abs( './' ) );
+    test.identical( filesBefore, filesAfter );
+    return null;
+  });
+  a.shell( 'git log -n 20' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, '__sync__' ), 0 );
+    test.ge( _.strCount( op.output, `Author: ${ user }` ), 0 );
+    return null;
+  });
+
+  /* */
+
+  begin().then( () =>
+  {
+    test.case = 'dstDirPath - nested dir, srcDirPath, dir synchronized';
+    return null;
+  });
+  a.appStart( `.agree dst:./!master src:../repo#${ srcState1 } dstDirPath:proto` );
+  a.appStart
+  (
+    `.migrate dst:./!master src:../repo!master srcState1:#${ srcState1 } srcState2:#${ srcState2 } `
+    + `dstDirPath:'proto' only:'[*package*, **.s]' onMessage:${ a.abs( '../OnMessage.js' ) }`
+  );
+  a.shell( 'git diff --name-only HEAD~..HEAD' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    var files = op.output.trim().split( '\n' );
+    test.identical( files.length, 2 );
+
+    var filesAfter = a.find( a.abs( './' ) );
+    test.identical( filesBefore, filesAfter );
+    return null;
+  });
+  a.shell( 'git log -n 20' );
+  a.ready.then( ( op ) =>
+  {
+    test.identical( op.exitCode, 0 );
+    test.identical( _.strCount( op.output, '__sync__' ), 8 );
+    test.ge( _.strCount( op.output, `Author: ${ user }` ), 8 );
+    return null;
+  });
+
+  /* - */
+
+  return a.ready;
+
+  /* */
+
+  function begin()
+  {
+    let code =
+`
+  function onMessage( msg )
+  {
+    return '__sync__';
+  }
+  module.exports = onMessage;
+`;
+    a.ready.then( () => { a.fileProvider.filesDelete( a.abs( '../OnMessage.js' ) ); return null });
+    a.ready.then( () => { a.fileProvider.fileWrite( a.abs( '../OnMessage.js' ), code ); return null });
+    a.ready.then( () => { a.fileProvider.filesDelete( a.abs( '.' ) ); return null });
+    a.ready.then( () => { a.fileProvider.filesDelete( a.abs( '../repo' ) ); return null });
+    a.ready.then( () => { a.fileProvider.dirMake( a.abs( '.' ) ); return null });
+    a.shell( `git clone ${ dstRepositoryRemote } ./` );
+    a.shell( `git reset --hard ${ dstCommit }` );
+    return a.shell( `git clone ${ srcRepositoryRemote } ../repo` );
+  }
+}
+
+migrateWithOptionDstDirPath.timeOut = 180000;
 
 //
 
@@ -2975,6 +3198,7 @@ const Proto =
     migrateWithOptionBut,
     migrateWithOptionOnly,
     migrateWithOptionSrcDirPath,
+    migrateWithOptionDstDirPath,
 
     commitsDates,
     commitsDatesWithOptionDelta,
