596de55b4820c226e784c187833b0c057f87a228
dmvict <dm.vict.kr@gmail.com>
2021-12-01 11:53:33 +0200

    Improve routine `repositoryMigrate`, add conditions, handle errors

--- proto/wtools/atop/repo/l6/RepoBasic.s
+++ proto/wtools/atop/repo/l6/RepoBasic.s
@@ -128,22 +128,21 @@ function repositoryMigrate( o )
   if( srcProvider.name === 'hd' )
   o.src = _.git.path.join( currentPath, o.src );
   o.dst = _.git.path.join( currentPath, o.dst );
+  const nativized = _.git.path.nativize( o.dst );
+
+  if( !_.git.isRepository({ localPath : _.git.path.nativize( o.dst ) }) )
+  throw _.error.brief( 'Destination path should be a repository. Please, input valid destination path.' )
 
   const srcParsed = _.git.path.parse( o.src );
-  _.sure( srcParsed.tag !== undefined );
-  const dstParsed = _.git.path.parse( o.dst );
-  _.sure( dstParsed.tag !== undefined, 'Expects defined branch in path {-dst-}' );
-
-  const nativized = _.git.path.nativize( o.dst );
-  const tagDescriptor = _.git.tagExplain
-  ({
-    remotePath : o.dst,
-    localPath : nativized,
-    tag : dstParsed.tag,
-    remote : 0,
-    local : 1,
-  });
-  _.sure( tagDescriptor.isBranch, `Expects branch but got tag ${ dstParsed.tag }` );
+  _.sure( srcParsed.tag !== undefined || srcParsed.hash !== undefined );
+  if
+  (
+    srcProvider.name === 'hd'
+    && _.git.isRepository({ localPath : _.git.path.nativize( o.src ) })
+    && srcParsed.tag === 'master'
+    && !_.str.has( o.src, _.git.path.tagToken )
+  )
+  srcParsed.tag = _.git.tagLocalRetrive({ localPath : o.src });
 
   let onCommitMessage = o.onMessage;
   if( onCommitMessage )
@@ -163,18 +162,27 @@ function repositoryMigrate( o )
     onDate = require( _.path.join( _.path.current(), onDate ) );
   }
 
-  return _.git.repositoryMigrate
-  ({
-    srcBasePath : o.src,
-    dstBasePath : nativized,
-    srcState1 : o.srcState1,
-    srcState2 : o.srcState2,
-    srcDirPath : o.srcDirPath,
-    dstDirPath : o.dstDirPath,
-    onCommitMessage,
-    onDate,
-    but : o.but,
-    only : o.only,
+  const ready = _.take( null ).then( () =>
+  {
+    return _.git.repositoryMigrate
+    ({
+      srcBasePath : o.src,
+      dstBasePath : nativized,
+      srcState1 : o.srcState1,
+      srcState2 : o.srcState2,
+      srcDirPath : o.srcDirPath,
+      dstDirPath : o.dstDirPath,
+      onCommitMessage,
+      onDate,
+      but : o.but,
+      only : o.only,
+    });
+  });
+  return ready.finally( ( err, arg ) =>
+  {
+    if( err )
+    throw _.error.brief( err );
+    return arg;
   });
 }
 
